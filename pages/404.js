832be5bb6081f30e4d0bfc9d027087ee06daa02b
danjarosz <jarosz.daniel@protonmail.com>
2021-06-12 11:17:13 +0200

    feat: auto redirect

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,21 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 
 const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // router.go(-1)
+      router.push("/");
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
     <div className="not-found">
       <h1>Ooooops...</h1>
